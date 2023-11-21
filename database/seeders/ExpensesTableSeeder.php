361a480e2ba6e3c5c220279829a2b2b9f7de840c
tvhima <hima@techvoot.com>
2023-11-21 23:13:21 +0530

    seeder added

---
+++ database/seeders/ExpensesTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Expense;
+
+class ExpensesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create sample expenses
+        Expense::create([
+            'title' => 'Expense 1',
+            'amount' => 500,
+            'date' => date('2002-05-13'),
+        ]);
+
+        Expense::create([
+            'title' => 'Expense 2',
+            'amount' => 150,
+            'date' => date('2020-03-13'),
+        ]);
+
+        Expense::create([
+            'title' => 'Expense 3',
+            'amount' => 550,
+            'date' => date('2002-10-13'),
+        ]);
+
+        Expense::create([
+            'title' => 'Expense 2',
+            'amount' => 190,
+            'date' => date('2020-04-13'),
+        ]);
+    }
+}
